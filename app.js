9c8df1286ffde50c8e319d52b8434e1935896ef5
Ayush Ujjwal <ujjwal.msrit@gmail.com>
2019-04-14 00:45:06 +0530

    Made changes in the app.js and the other files

--- app.js
+++ app.js
@@ -1,2 +1,39 @@
-var express=reqiure('express'),
-    bodyParser=require('body-parser')
+var express=require('express'),
+    bodyParser = require('body-parser'),
+    mongoose    =require('mongoose'),
+    app=express();
+
+mongoose.connect("mongodb://localhost/keep_i");
+
+app.set("view engine","ejs");
+
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+//getting the routes done 
+app.get("/",function(req,res){
+    res.render("index")
+})
+
+
+app.get("/login",function(req,res){
+    res.render("login")
+})
+
+app.get("/login/choose",function(req,res){
+    res.render("choose");
+})
+
+app.get("/login/choose/postelec",function(req,res){
+    res.render("postelec");
+})
+
+// app.post("/login/choose/postelec",function(req,res){
+
+// })
+
+PORT=8000;
+app.listen(PORT,process.env.IP,function(){
+    console.log("Ready to go");
+})
